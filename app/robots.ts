ac70ea8605223cd45605e74b4c68fa1a9cc3b8dd
Jamiu10012 <ibrahimjamiu026@gmail.com>
2026-02-02 11:01:48 +0100

    Added new robo

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,12 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    sitemap: "https://freefoundryhub.com//sitemap.xml",
+    rules: [
+      {
+        userAgent: "*", // Allows all user agents (bots)
+        allow: "/", // Allows crawling of all pages
+      },
+    ],
+    sitemap: "https://freefoundryhub.com/sitemap.xml", // Corrected sitemap URL
   };
 }
